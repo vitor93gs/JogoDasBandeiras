e54db80dfff4c924c21ad6a9ca03de00521a1842
Vitor Santos <vitor93gs@gmail.com>
2022-01-21 15:25:58 -0300

    Atualizacao HTML e teste de array

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
-const ArrJogo = []
+const arrJogo = []
 for(let i = 0; i<capital.length;i++){
-    ArrJogo.push(new Pais(nome[1],bandeira[1],capital[1]))
+    arrJogo.push(new Pais(nome[i],bandeira[i],capital[i]))
 }
+console.log(arrJogo)
