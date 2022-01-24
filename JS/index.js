a640e0b5cabe2d26485ec112c574f227aa0beb84
Vitor Santos <vitor93gs@gmail.com>
2022-01-24 11:12:05 -0300

    inclusao de selecionador de efeito de selecionador de dificuldade e de tipo de jogo, show/no-show de jogo e setup, e inicio de configuracao de CSS

--- JS/index.js
+++ JS/index.js
@@ -10,12 +10,81 @@ let tentativas = 0;
 let nomeDeUsuario = "";
 let dificuldade = "";
 let tamDoArr = 164;
+let tempo = 500;
+let idTempo = "";
+let modoDeJogo = "";
 const botao1 = document.getElementById("btn1");
 const botao2 = document.getElementById("btn2");
 const botao3 = document.getElementById("btn3");
 const botao4 = document.getElementById("btn4");
+const timerDoJogo = document.getElementById("timer");
 const img = document.getElementById("img1");
+const btnFacil = document.getElementById("facil");
+const btnMedio = document.getElementById("medio");
+const btnDificil = document.getElementById("dificil");
+const btnBandeiras = document.getElementById("bandeiras");
+const btnCapitais = document.getElementById("capitais");
+const btnInvertido  = document.getElementById("invertido");
+const btnJogar = document.getElementById("jogar");
+const tela1 = document.getElementById("tela1");
+const tela2 = document.getElementById("tela2");
+const voltar = document.getElementById("return");
 
+// Logica dos botoes de escolha de tipo de jogo e de dificuldade
+function selecionadoDificuldade(botao){
+    btnFacil.classList.remove("selected");
+    btnMedio.classList.remove("selected");
+    btnDificil.classList.remove("selected");
+    botao.classList.add("selected");
+    if(botao.innerText === "Facil"){
+        tamDoArr = 20;
+    }
+    if(botao.innerText === "Medio"){
+        tamDoArr = 80;
+    }
+    else{
+        tamDoArr = 164;
+    }
+}
+selecionadoDificuldade(btnFacil);
+
+function selecionadoTipo (botao){
+    btnCapitais.classList.remove("selected");
+    btnBandeiras.classList.remove("selected");
+    btnInvertido.classList.remove("selected");
+    botao.classList.add("selected");
+    // if(botao.innerText === "Facil"){
+    //     tamDoArr = 20;
+    // }
+    // if(botao.innerText === "Medio"){
+    //     tamDoArr = 80;
+    // }
+    // else{
+    //     tamDoArr = 164;
+    // }
+}
+selecionadoTipo(btnBandeiras);
+
+//BOTOES FUNCIONANDO
+
+btnDificil.addEventListener('click', () => {
+    selecionadoDificuldade(btnDificil)  
+});
+btnFacil.addEventListener('click', () => {
+    selecionadoDificuldade(btnFacil)  
+});
+btnMedio.addEventListener('click', () => {
+    selecionadoDificuldade(btnMedio)  
+});
+btnInvertido.addEventListener('click', () => {
+    selecionadoTipo(btnInvertido)  
+});
+btnBandeiras.addEventListener('click', () => {
+    selecionadoTipo(btnBandeiras)  
+});
+btnCapitais.addEventListener('click', () => {
+    selecionadoTipo(btnCapitais)  
+});
 // Funcao para conseguir um numero aleatorio dentro do array
 
 function numAleatorio(tam){
@@ -37,5 +106,45 @@ function selecionarPaises(){
 }
 const arrJogo1 = selecionarPaises()
 
+// funcao timer de 5 segundos para o jogo
+function timer(){
+    idTempo = setInterval(() => { 
+        tempo --;
+        timerDoJogo.innerText = tempo;
+        if(tempo === 0){
+            clearInterval(idTempo);
+            tempo = 500;
+        }    
+    }, 10);    
+}
+
+//funcao que comeca o jogo
+
+btnJogar.addEventListener('click', () => {
+    tela1.classList.remove("show");
+    tela1.classList.add("no_show");
+    tela2.classList.remove("no_show");
+    tela2.classList.remove("show");
+});
+
+// funcao que volta para a tela inicial
+function resetAll(){
+    tamDoArr = 164;
+    tempo = 500;
+    tela2.classList.remove("show");
+    tela2.classList.add("no_show");
+    tela1.classList.remove("no_show");
+    tela1.classList.remove("show");
+}
+
+//botoes de retorno
+voltar.addEventListener('click', () => {
+    resetAll()
+});
+
+
+
+
+img.src = arrJogo[1].bandeira
 console.log(arrJogo[1].bandeira)
 
